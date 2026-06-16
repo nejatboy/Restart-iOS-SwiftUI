719ccb942ca0a22452482d9b46bcd056ad00215e
Nejat Boy <>
2026-06-17 00:53:23 +0300

    Project created.

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,18 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Nejat Boy on 17.06.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
